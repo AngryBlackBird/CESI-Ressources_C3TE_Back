c4f39666f8adce58cb15ed43124c683314627f3d
blackbird <t.morabire@gmail.com>
2024-01-18 16:49:04 +0100

    wip auth

---
+++ src/Controller/ApiLoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use App\Entity\User;
+
+class ApiLoginController extends AbstractController
+{
+    #[Route('/api/login', name: 'api_login')]
+    public function login(#[CurrentUser] ?User $user): JsonResponse
+    {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        return $this->json([
+            'user' => $user->getUserIdentifier()]);
+    }
+}
