f3dd65ba1ba4c53e732fa9eaaebcc27e2a78037f
Thibault <t.morabire@gmail.Com>
2024-03-06 16:01:29 +0100

    add info in jwt

--- src/EventListener/JWTCreatedListener.php
+++ src/EventListener/JWTCreatedListener.php
@@ -1,36 +1,16 @@
 <?php
 
 namespace App\EventListener;
-// src/App/EventListener/JWTCreatedListener.php
 
 use App\Entity\User;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
-use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 final class JWTCreatedListener
 {
-    private Security $security;
-
-    /**
-     * @param RequestStack $requestStack
-     */
-    public function __construct(Security $security)
+    public function onJWTCreated(JWTCreatedEvent $event): void
     {
-        $this->security = $security;
-    }
-
-    /**
-     * @param JWTCreatedEvent $event
-     *
-     * @return void
-     */
-    public function onJWTCreated(JWTCreatedEvent $event)
-    {
-        /**
-         * @var User $currentUser
-         */
-        $currentUser = $this->security->getUser();
+        /** @var User $currentUser */
+        $currentUser = $event->getUser();;
         $payload = $event->getData();
 
         $payload['firstName'] = $currentUser->getFirstName();
