d81c3dab7620eddff5066c0b136400085c67a1ed
Thibault <t.morabire@gmail.Com>
2024-03-07 14:56:02 +0100

    test unit

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,12 +2,35 @@
 
 namespace App\Tests;
 
+use App\Entity\Resource;
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    public function testSomething(): void
+    private bool $failure = false;
+
+    public function testAddResource(): void
     {
-        $this->assertTrue(true);
+        $user = new User();
+        $resource = new Resource();
+
+        $user->addResource($resource);
+
+        $this->assertTrue($user->getResources()->contains($resource));
+    }
+
+    public function testRemoveResource(): void
+    {
+        $user = new User();
+        $resource = new Resource();
+
+        $user->addResource($resource);
+        $user->removeResource($resource);
+        if ($this->failure) {
+            $this->fail('testRemoveResource falure');
+
+        }
+        $this->assertFalse($user->getResources()->contains($resource));
     }
 }
